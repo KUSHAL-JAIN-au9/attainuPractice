655e6f570ff6d8934a3f3e028f3d821ac876198a
Kushal-Jain-au8 <jkushal069@gmail.com>
2021-02-18 11:27:01 +0530

    deleted unrequired files

--- db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-
-const config = {
-    autoIndex: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  };
-mongoose.connect('mongodb://localhost:27017/aryablog',config);
